5c9d55b7348282c28af3c76e15d732cbc4a030a5
Jeong-il Suk <wjddlf211@naver.com>
2025-07-28 16:36:31 +0900

    chore: dist가 무시되게 하는 eslint설정 고침

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,6 @@ export default defineConfig([
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
-    ignores: ["**/temp.js", "config/*", "dist", ".eslintrc.js"],
+    ignores: ["**/temp.js", "config/*", "dist/", ".eslintrc.js"],
   },
 ]);
