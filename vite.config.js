01dbcd4059b66f204f81e2a6d3f595a813df9cf7
Jeong-il Suk <wjddlf211@naver.com>
2025-08-01 16:27:15 +0900

    refactor: home -> index

--- vite.config.js
+++ vite.config.js
@@ -42,6 +42,8 @@ export default defineConfig({
   },
   appType: "spa", // fallback 사용안함
   server: {
-    // open: 'src/pages/main/index.html', // 서버 시작 시 브라우저에서 지정페이지 자동으로 열기
+    server: {
+      https: true,
+    },
   },
 });
